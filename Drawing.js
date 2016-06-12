c6ae181a30038882fa726f4dab2d8ebf739829ea
mildlywarm <Tim@Odin.local>
2016-06-11 23:22:05 -0700

    We got drawing
    -Able to draw on the screen
    -Canvas isn’t the proper size
    -line is not lined up with mouse

--- Drawing.js
+++ Drawing.js
@@ -1,54 +1,38 @@
 var canvas = document.getElementById("Canvas"); 
-var canvas_Context = canvas.getContext("2d"); 
- var clutter = false;
+var canvas_Context = canvas.getContext("2d");   
 
-//mouse position
-//var pos = canvas.input.activePointer.position;
-var space = canvas.getBoundingClientRect(); 
-/*
-var xval = document.getElementById("X"); 
-var yval = document.getElementById("Y"); 
-var pos = getMousePos(canvas, e);
+//Drawing tells if mouse button has been pressed. 
+var Drawing; 
 
 
 
-xval = pos.x; 
-yval = pos.y; 
-*/
 
 
-function draw(event){
+//if mouse has been pressed, move to mouse location, and set drawing to true. 
+canvas.onmousedown = function (e) {
     
+    canvas_Context.moveTo(e.clientX, e.clientY); 
     
+        Drawing = true; 
+    
+}
 
-    
-    if(clutter === false){
-    
-        canvas_Context.beginPath(); 
-        canvas_Context.lineWidth = "5"; 
-        canvas_Context.strokeStyle = "red"; 
-        canvas_Context.moveTo(0, 90); 
-        canvas_Context.lineTo(300, 175); 
-        canvas_Context.stroke(); 
-        //Just want to get things to show up on the canvas. 
-        canvas_Context.beginPath(); 
-        canvas_Context.lineWidth = "5";
-        canvas_Context.strokeStyle = "green"; // Green path
-        canvas_Context.moveTo(0, 75);
-        canvas_Context.lineTo(250, 75);
-        canvas_Context.stroke(); // Draw it
+//while mouse is pressed draw
+canvas.onmousemove = function (e) {
 
-        canvas_Context.beginPath();
-        canvas_Context.strokeStyle = "purple";  // Purple path
-        canvas_Context.moveTo(50, 0);
-        canvas_Context.lineTo(150, 130);            
-        canvas_Context.stroke();  // Draw it
-        clutter = true; 
+    if(Drawing) {
         
-    } else if (clutter === true) {
-        
-        canvas_Context.clearRect(0, 0, canvas.width, canvas.height);
-        clutter = false; 
-        
+        canvas_Context.lineTo(e.clientX, e.clientY); 
+        canvas_Context.stroke();
     }
 }
+//when mouse is released stop drawing
+canvas.onmouseup = function (e) {
+    
+        Drawing = false; 
+    
+}
+
+
+
+
