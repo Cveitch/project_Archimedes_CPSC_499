a0c85446a76635a1ff3690d6dcc5c36500cc9445
mildlywarm <Tim@Odin.local>
2016-06-12 22:07:14 -0700

    No backwards funk
    Line can only be drawn one way now, although this is super finicky and
    seems like its more trouble then its worth. Also stores X values in an
    array.

--- Drawing.js
+++ Drawing.js
@@ -2,6 +2,11 @@
 var canvas = document.getElementById("Canvas"); 
 var canvas_Context = canvas.getContext("2d");
 
+// array for x values
+var xValue = [0]; 
+var i = 0; 
+
+
 //checks to see if the window gets resized
 window.addEventListener('resize', canvas_Size, false);
 
@@ -28,31 +33,50 @@ canvas.onmousemove = function (e) {
 
     if(Drawing) {
         
-        canvas_Context.lineTo(e.pageX-this.offsetLeft, e.pageY-this.offsetTop); 
+        // canvas_Context.lineTo(e.pageX-this.offsetLeft, e.pageY-this.offsetTop); 
+        //add X value to array 
+        xValue.push(e.pageX-this.offsetLeft); 
         
-        canvas_Context.stroke();
-       
-       
+        //if Xvalue is less than current X value, then draw
+            if(xValue[i]<e.pageX-this.offsetLeft){
+            
+                canvas_Context.lineTo(e.pageX-this.offsetLeft, e.pageY-this.offsetTop); 
+                canvas_Context.stroke();
+                i++;  
+            //if Xvalue is greater or equal to current X then it means the user is going backwards and thats trouble.
+            //So lets erase the line and make them start over. 
+            } else if(xValue[i] >= e.pageX-this.offsetLeft) {
+            //Disable drawing
+            Drawing = false; 
+            //clear canvas
+            canvas_Context.clearRect(0, 0, canvas.width, canvas.height);
+            canvas_Context.beginPath();
+            //reset Xvalue array
+            xValue.length = 1; 
+            i = 0; 
+            
+        }
+        
     }else{
         canvas_Context.moveTo(e.pageX-this.offsetLeft, e.pageY-this.offsetTop); 
 
     }
 }
-//when mouse is released stop drawing
+    //when mouse is released stop drawing
 canvas.onmouseup = function (e) {
-   // canvas_Context.moveTo(e.pageX-this.offsetLeft, e.pageY-this.offsetLeft);
+    // canvas_Context.moveTo(e.pageX-this.offsetLeft, e.pageY-this.offsetLeft);
         Drawing = false; 
     
 }
 
-//If mouse leaves the canvas, then it stops drawing
+    //If mouse leaves the canvas, then it stops drawing
 canvas.onmouseleave = function (e){
         Drawing = false; 
 }
 
 
 
-//dynamic canvas size
+    //dynamic canvas size
 var canvas_Size = function(){
     
     canvas.height = window.outerHeight-150; 
@@ -64,13 +88,23 @@ var canvas_Size = function(){
 
 
 
+
+
+
+
+
+
+
+
+
+
  /* random notes
         
         
         todo: 
         X-Canvas should be the size of the screen. Right now its only as big as I make it. 
-        -Line does not start where the mouse cursor is. I think this has something to do with how the x and y values are being measured. 
-        -Not be able to draw backwards. Think I can just check for the mouse x values and compare them to the previous values. 
+        X-Line does not start where the mouse cursor is. I think this has something to do with how the x and y values are being measured. 
+        X-Not be able to draw backwards. Think I can just check for the mouse x values and compare them to the previous values. 
         -Play around with storing x,y values. 
         
         
