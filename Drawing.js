414050146c2541a33527c6d0afdc62dd7a8718b9
mildlywarm <Tim@Odin.local>
2016-06-12 21:01:09 -0700

    Stops drawing when mouse leaves the canvas
    If the mouse leaves the canvas area, then it stops drawing.

--- Drawing.js
+++ Drawing.js
@@ -15,7 +15,7 @@ var Drawing;
 //if mouse has been pressed, move to mouse location, and set drawing to true. 
 canvas.onmousedown = function (e) {
     
-    canvas_Context.moveTo(e.clientX, e.clientY); 
+    canvas_Context.moveTo(e.pageX-this.offsetLeft, e.pageY-this.offsetLeft); 
     
         Drawing = true; 
     
@@ -26,9 +26,49 @@ canvas.onmousemove = function (e) {
 
     if(Drawing) {
         
-        canvas_Context.lineTo(e.clientX, e.clientY); 
+        canvas_Context.lineTo(e.pageX-this.offsetLeft, e.pageY-this.offsetTop); 
         canvas_Context.stroke();
-        /*
+       
+       
+    }
+}
+//when mouse is released stop drawing
+canvas.onmouseup = function (e) {
+    
+        Drawing = false; 
+    
+}
+
+//If mouse leaves the canvas, then it stops drawing
+canvas.onmouseleave = function (e){
+        Drawing = false; 
+}
+
+
+
+//dynamic canvas size
+var canvas_Size = function(){
+    
+    canvas.height = window.outerHeight-150; 
+    canvas.width = window.outerWidth-30; 
+    
+    
+}
+
+
+
+
+ /* random notes
+        
+        
+        todo: 
+        X-Canvas should be the size of the screen. Right now its only as big as I make it. 
+        -Line does not start where the mouse cursor is. I think this has something to do with how the x and y values are being measured. 
+        -Not be able to draw backwards. Think I can just check for the mouse x values and compare them to the previous values. 
+        -Play around with storing x,y values. 
+        
+        
+        
         
          can take values and store them in data structure. 
         array? 
@@ -48,25 +88,3 @@ canvas.onmousemove = function (e) {
         
         
         */
-       
-    }
-}
-//when mouse is released stop drawing
-canvas.onmouseup = function (e) {
-    
-        Drawing = false; 
-    
-}
-
-//dynamic canvas size
-var canvas_Size = function(){
-    
-    canvas.height = window.outerHeight-150; 
-    canvas.width = window.outerWidth-30; 
-    
-    
-}
-
-
-
-
