e74624a3764f4b7fd5215dbd336f6013ee83204d
mildlywarm <Tim@Odin.local>
2016-06-12 20:40:48 -0700

    Dynamic resizing of canvas area
    Canvas will now change to fit the screen.

--- Drawing.js
+++ Drawing.js
@@ -1,5 +1,9 @@
+//get canvas information
 var canvas = document.getElementById("Canvas"); 
-var canvas_Context = canvas.getContext("2d");   
+var canvas_Context = canvas.getContext("2d");
+
+//checks to see if the window gets resized
+window.addEventListener('resize', canvas_Size, false);
 
 //Drawing tells if mouse button has been pressed. 
 var Drawing; 
@@ -24,12 +28,42 @@ canvas.onmousemove = function (e) {
         
         canvas_Context.lineTo(e.clientX, e.clientY); 
         canvas_Context.stroke();
+        /*
+        
+         can take values and store them in data structure. 
+        array? 
+        2d array [x,y]
+        Only 1 of each x value
+        x is gong to be plus one each time, its never going to go backwards
+        Y does not matter
+        really only need x values? ---maybe
+        might be to strict? 
+        BUT WHATS THE SCALE???????
+        It cant be 1 pixel = 1 value (x or y)
+        
+        Scale?
+        Divide screen length into 10? 
+        Divide screen length 
+        
+        
+        
+        */
+       
     }
 }
 //when mouse is released stop drawing
 canvas.onmouseup = function (e) {
     
         Drawing = false; 
+    
+}
+
+//dynamic canvas size
+var canvas_Size = function(){
+    
+    canvas.height = window.outerHeight-150; 
+    canvas.width = window.outerWidth-30; 
+    
     
 }
 
