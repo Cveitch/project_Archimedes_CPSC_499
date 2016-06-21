db4cee72115506eb0bc1068eb936d040cfd1f71e
RileyWium <wium@unbc.ca>
2016-06-20 23:47:05 -0700

    Added level 1, and some physics implementation
    This branch is for the creation of a few test levels and getting those test levels to run as intended on the browser. This is not to be confused with implementing a level (or stage) traversal system.

--- js/Main.js
+++ js/Main.js
@@ -6,28 +6,57 @@ var Main = function(game){
 Main.prototype = {
 
 	create: function() {
-
-		//sets a background (which doesn't want to work somehow...)
-		background = game.add.sprite(0,0,"background");
-		background.scale.setTo(2);
+		
+		// Start the P2 Physics Engine
+		this.game.physics.startSystem(Phaser.Physics.P2JS);
+		// Set the gravity
+		this.game.physics.p2.gravity.y = 900;
+		
+		// initialised tilemap with matching tileset
+		var mymap = this.game.add.tilemap('testmap');
+		mymap.addTilesetImage('tset_world1');
 
 		//temporary colour for the background
-		this.game.stage.backgroundColor = "71c5cf";
+		//this.game.stage.backgroundColor = "71c5cf";
+		
+		//creates layers matching the .json testlevel 
+	    layerbackground = mymap.createLayer('background');
+		layerblocks 	= mymap.createLayer('block1');
+		layerdetails 	= mymap.createLayer('detail1');
+	
+		//we resize the world to the background as it will be covering the entire level
+	    layerbackground.resizeWorld();
 
-	    // Start the P2 Physics Engine
-	    this.game.physics.startSystem(Phaser.Physics.P2JS);
+		//this sets both the block and detail layer's collision to true
+			//mymap.setCollisionByExclusion([0],true,'layerblocks');
+			//mymap.setCollisionBetween(1,20,true,'layerdetails');
 
-	    // Set the gravity
-	    this.game.physics.p2.gravity.y = 1000;
-
-		// Create the ceiling
-	    //this.createBlock();
-
-		//Doesn't work now...
-		//this.createSlope();
+		//uses p2 to make multiple bodies in tile maps into solid single body
+		layerblocks_tiles 	= this.game.physics.p2.convertTilemap(mymap,layerblocks);
+		 layerdetails 	= this.game.physics.p2.convertTilemap(mymap, layerdetails);
+		//turns polylines solid
+		layerpolyline_tiles = this.game.physics.p2.convertCollisionObjects(mymap, "objects2");
 
 	    // Create the player
-	    this.createPlayer();
+	    //this.createPlayer();
+
+		// Add the player to the game
+		this.player = this.game.add.sprite(0, 0, "avatar");
+		this.game.physics.p2.enable(this.player);
+		//quality of life settings 
+		this.player.anchor.setTo(0.5,0.5);
+		this.game.camera.follow(this.player);
+		//gives player a circle hitbox (radius,offestx,offsety)
+		this.player.body.setCircle(22,0,0);
+		//wouldn't want the character tumbling over
+		this.player.body.fixedRotation=true;
+		
+		
+		// Add goal to the game
+		goal = this.game.add.sprite(520,400,"goal");
+		
+		//this allows for real time in game control with keyboard, thanks to the update function 
+		cursors = this.game.input.keyboard.createCursorKeys();
 	},
 
 	createBlock: function() {
@@ -75,26 +104,49 @@ Main.prototype = {
 
 	createPlayer: function() {
 
-	    // Add the player to the game
-	    this.player = this.game.add.sprite(200, 400, "betty");
-
+	   
+		
+		
 	    // Enable physics, use "true" to enable debug drawing
-	    this.game.physics.p2.enable([this.player], false);
+	    //this.game.physics.p2.enable([this.player], false);
 
 	    // Get rid of current bounding box
-	    this.player.body.clearShapes();
+	    //this.player.body.clearShapes();
 
 	    // Add our PhysicsEditor bounding shape (causes betty to have NOT fly out of the page)
-	    this.player.body.loadPolygon("sprite_physics", "betty");
+	    //this.player.body.loadPolygon("sprite_physics", "betty");
 
-		var spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-		spaceKey.onDown.add(this.jump, this);
+		//var spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+		//spaceKey.onDown.add(this.jump, this);
+		
+		
 	},
 
 	update: function() {
 		//updates the game if betty dies or exceeds boundaries
+		
+		//cursors is used for in game control as an example of physics capabilities
+		
+		this.player.body.velocity.x = 0;
+		
+		if 		(cursors.left.isDown)
+		{//move left, flip character left
+			this.player.scale.x = -1;
+			this.player.body.velocity.x = -300;
+		}
+		else if (cursors.right.isDown)
+		{//move right, flip right
+			this.player.scale.x = 1;
+			this.player.body.velocity.x = 300;
+		}
+		if(cursors.up.isDown)
+		{//jumps
+			this.player.body.velocity.y = -800;
+		}
+		
 	},
-
+	
+	//can set controls in update so (Riley) moved them their, this function not called
 	jump: function() {
 		//preform jump
 		this.player.body.velocity.y = -350;
