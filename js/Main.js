de8e36441f8ada03eec903eb22f8d4b4bf5375f7
avrobullet <douglasa@unbc.ca>
2016-06-20 23:13:57 -0700

    Sprite with Changing Velocities Implemented
    Got the alternate velocities to work. The only thing to plug in is the ability to load up the queue created from the canvas page into getSpeed() and voila!

--- js/Main.js
+++ js/Main.js
@@ -1,108 +1,173 @@
 
-var Main = function(game){
+var Main = function(game)
+{
 	//This function allows "Main" to be accessed by the game instance
 };
 
 Main.prototype = {
 
-	create: function() {
-
-		//sets a background (which doesn't want to work somehow...)
-		background = game.add.sprite(0,0,"background");
+	create: function()
+	{
+		//Sets a background (which can't seem to be tiled...)
+		var background = game.add.sprite(0,0,"background");
 		background.scale.setTo(2);
 
-		//temporary colour for the background
+		//Index for the queue/array
+		this.arrayIndex = 0;
+
+		//Time interval value to determine when to pull from queue/array
+		this.arrayMoment = 0;
+
+		//Current speed for the sprite
+		this.nextSpeed = 0;
+
+		//Puts the index of the queue/array on display (TESTING)
+		this.labelIndex = game.add.text(20, 20, "0",{ font: "30px Arial", fill: "#ffffff" });
+
+		//Allows getSpeed() to retrieve the next speed value that the sprite will move at
+		this.currentState = false;
+
+		//Temporary colour for the background
 		this.game.stage.backgroundColor = "71c5cf";
 
-	    // Start the P2 Physics Engine
+	    //Start the P2 Physics Engine
 	    this.game.physics.startSystem(Phaser.Physics.P2JS);
 
-	    // Set the gravity
+	    //Set the gravity
 	    this.game.physics.p2.gravity.y = 1000;
 
-		// Create the ceiling
-	    //this.createBlock();
+		//Create the ceiling
+	    this.createBlock();
 
-		//Doesn't work now...
-		//this.createSlope();
+		//Winning condition
+		this.winLevel();
 
-	    // Create the player
+	    //Create the player
 	    this.createPlayer();
 	},
 
-	createBlock: function() {
-
-	    // Define a block using bitmap data rather than an image sprite
-	    var blockShape = this.game.add.bitmapData(this.game.world.width, 200);
-
-	    // Fill the block with black color
-	    blockShape.ctx.rect(0, 0, this.game.world.width, 200);
-	    blockShape.ctx.fillStyle = "71c5cf";
-	    blockShape.ctx.fill();
-
-	    // Create a new sprite using the bitmap data
-	    this.block = this.game.add.sprite(0, 0, blockShape);
-
-	    // Enable P2 Physics and set the block not to move
-	    this.game.physics.p2.enable(this.block);
-	    this.block.body.static = true;
-	    this.block.anchor.setTo(0, 0);
+	update: function()
+	{
+		//Updates sprite speed
+		this.movePlayer(this.getSpeed());
 	},
 
-	/*
+	createBlock: function()
+	{
+		//Define a block using bitmap data rather than an image sprite
+		var blockShape = this.game.add.bitmapData(this.game.world.width, 200);
 
-	<!--The following is under development. Creating a slope of sorts should be made in a separate .js where the main focus
-	is to generate the landscape for the sprite to run on.-->
+		//Fill the block with black color
+		blockShape.ctx.rect(0, 0, this.game.world.width, 200);
+		blockShape.ctx.fillStyle = "000000";
+		blockShape.ctx.fill();
 
-	createSlope: function(){
-	// Define a block using bitmap data rather than an image sprite
-	var slopeShape = this.game.add.bitmapData(this.game.world.width, 200);
+		//Create a new sprite using the bitmap data and place it at (x,y) location
+		this.block = this.game.add.sprite(this.game.world.width/2,this.game.world.height, blockShape);
 
-	// Fill the block with black color
-	slopeShape.ctx.rect(0, 0, this.game.world.width, 100);
-	slopeShape.ctx.fillStyle = '255';
-	slopeShape.ctx.fill();
-
-	// Create a new sprite using the bitmap data
-	this.slope = this.game.add.sprite(0, 0, slopeShape);
-
-	// Enable P2 Physics and set the block not to move
-	this.game.physics.p2.enable(this.slope);
-	this.slope.body.static = false;
-	this.slope.anchor.setTo(0, 0);
+		//Enable P2 Physics and set the block not to move
+		this.game.physics.p2.enable(this.block);
+		this.block.body.static = true;
 	},
-	* */
 
-	createPlayer: function() {
-
-	    // Add the player to the game
+	createPlayer: function()
+	{
+	    //Add the player to the game
 	    this.player = this.game.add.sprite(200, 400, "betty");
 
-	    // Enable physics, use "true" to enable debug drawing
-	    this.game.physics.p2.enable([this.player], false);
+	    //Enable physics, use "true" to enable debug drawing
+	    //this.game.physics.p2.enable([this.player], false);
+		this.game.physics.p2.enable(this.player);
 
-	    // Get rid of current bounding box
+
+	    //Get rid of current bounding box
 	    this.player.body.clearShapes();
 
-	    // Add our PhysicsEditor bounding shape (causes betty to have NOT fly out of the page)
+	    //Add our PhysicsEditor bounding shape (causes betty to have NOT fly out of the page)
 	    this.player.body.loadPolygon("sprite_physics", "betty");
 
+		//Initializing player's boundary to generate a response with the block
+		var playerMaterial = this.game.physics.p2.createMaterial('playerMaterial', this.player.body);
+	},
+
+	movePlayer: function()
+	{
+		//Makes sprite jump (temporary measure)
 		var spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 		spaceKey.onDown.add(this.jump, this);
+
+		//If player is in contact with a slope
+		//-->code
+
+		//If the queue is empty OR if the velocity is 0
+		if(this.nextSpeed === 0)
+		{
+			//Sprite speed
+			this.player.body.velocity.x = 150;
+		}
+		else
+		{
+			this.player.body.velocity.x = this.nextSpeed;
+			//var newSpeed = this.player.accerelate(speed);
+		}
 	},
 
-	update: function() {
-		//updates the game if betty dies or exceeds boundaries
+	winLevel: function()
+	{
+		//Define a block using bitmap data rather than an image sprite
+		var blockEnd = this.game.add.bitmapData(100, 200);
+
+		//Fill the block with black color
+		blockEnd.ctx.rect(0, 0, 100, 200);
+		blockEnd.ctx.fillStyle = "ff0000";
+		blockEnd.ctx.fill();
+
+		//Create a new sprite using the bitmap data and place it at (x,y) location
+		this.blockE = this.game.add.sprite(this.game.world.width-100,this.game.world.height-200, blockEnd);
+
+		//Enable P2 Physics and set the block not to move
+		this.game.physics.p2.enable(this.blockE);
+		this.blockE.body.static = true;
 	},
 
-	jump: function() {
+	jump: function()
+	{
 		//preform jump
 		this.player.body.velocity.y = -350;
 	},
 
-	restartGame: function(){
-		// restart the game after death
-		this.game.state.start('main');
+	getSpeed: function()
+	{
+		//Retrieve queue/array of the speed values
+		var speedValues = [150, -150, 450, 450, 450];
+
+		//Checks every 50 cycles to pull from queue/array
+		if(this.arrayMoment % 50 === 0)
+		{
+			//if(speedValues[this.arrayIndex] !== null)
+			if(this.arrayIndex < speedValues.length)
+			{
+				this.nextSpeed = speedValues[this.arrayIndex];
+			}
+			else
+			{
+				//sets the speed to the degault setting
+				this.nextSpeed = 0;
+			}
+
+			this.arrayIndex += 1;
+		}
+
+		//Update arrayMoment
+		this.arrayMoment += 1;
+
+		//Display the current velocity
+		this.labelIndex.text =  "step..." + this.arrayIndex;
 	},
 
+	accelerate: function()
+	{
+		//Set new speed based on retrieved data from queue stored in local memory
+		//this.player.velocity.x = newSpeed;
+	},
 };
