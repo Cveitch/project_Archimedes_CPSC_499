234fedc4a64fbf6fbcabb1293f59cea64c691c9d
avrobullet <douglasa@unbc.ca>
2016-06-22 19:19:51 -0700

    Sprite changes: JS
    The required js files that enable proper velocity changes based per update cycle of 50.

--- js/Buttons.js
+++ js/Buttons.js
@@ -2,36 +2,25 @@
  * PROJECT_ARCHIMEDES 2016.
  */
 
-var jump = true;
-
-//AD: this method doesn't work
 function CONTEXT_SWITCH(PAGE_VAL){
 //    Allows for changing of pages
     console.log(PAGE_VAL);
     switch(PAGE_VAL) {
         case "SPLASH":
-            window.location.href = "../../project_Archimedes_CPSC_499-ALEX_1/Splash_Page.html";
+            window.location.href = "../Splash_Page.html";
             break;
         case "SPRITE":
-            window.location.href = "window.location.href='Canvas_Page.html";
+            window.location.href = "../Canvas_Page.html";
             break;
         case "CANVAS":
-            window.location.href = "../../project_Archimedes_CPSC_499-ALEX_1/Sprite_Page.html";
+            window.location.href = "../Sprite_Page.html";
             break;
         case "SCORE":
-            window.location.href = "../../project_Archimedes_CPSC_499-ALEX_1/Score_Page.html";
+            window.location.href = "../Score_Page.html";
             break;
         default:
-            window.location.href = "../../project_Archimedes_CPSC_499-ALEX_1/Score_Page.html";
+            window.location.href = "../Score_Page.html";
             break;
     }
     
 }
-
-function setCompare(text){
-    if(text === "FALSE"){
-        jump = false;
-    }
-}
-
-function getCompare(){ return jump }
