844082b12321debefdc78ead709a6a2b30daf7aa
DavisGoulet <davis.goulet@gmail.com>
2016-06-29 20:55:50 -0700

    Moved functions for splash page into own file and this now holds all user related functions.

--- UserInfo.js
+++ UserInfo.js
@@ -2,53 +2,128 @@
  * Created by DavisGoulet on 2016-06-11.
  */
 
-
 /**
- * Take the user input from the splash page and if it is correct,
- * saves it and moves on to the next page.
- * @param {String} buttonID
+ * Stores the username that the user gave to session storage.
+ * @param {String} name
  */
-function signIn(buttonID) 
+function setUsername(name)
 {
-    var name;
-
-    //Gets the name of the user from the options that they select on the splash screen
-    //If the user clicks the guest button, the assign the name as guest, else sign in as the username they gave
-    if(buttonID === "continueGuestButton")
-    {
-        name = "Guest";
-    }
-    else if(buttonID === "signInButton")
-    {
-        name = document.getElementById("userNameInput").value;
-
-        //Checks that the username length is not less then 2 or greater then 20 characters.
-        if(name.length < 2 || name.length > 20)
-        {
-            document.getElementById("invalidLengthLabel").style.visibility = "visible";
-            return;
-        }
-    }
-    //If for some reason this function is called from neither of the buttons, then prevent it from continuing on
-    else
-    {
-        return;
-    }
-
     //Saves the username in the session storage so that other pages can retrieve it.
     sessionStorage.setItem("Username",name);
-
-    //Continue on to the next page
-    window.location.href = "Sprite_Page.html";
-
-
 }
 
 /**
  * Sets the text of the container given by the id to the username that the user gave.
  * @param {String} container
  */
-function getUsername(container)
+function displayUsername(container)
 {
-    document.getElementById(container).innerHTML = sessionStorage.getItem("Username");
+    //Sets the container text with given id to the username.
+    document.getElementById(container).innerHTML = getUsername();
+}
+
+/**
+ * Returns the username. If no name is given then a default name of guest is assigned.
+ */
+function getUsername()
+{
+    //Gets the variable saved to the session storage under the name "username".
+    var name = sessionStorage.getItem("Username");
+
+    //If the username is null, then assign a it the default value of "Guest".
+    if(name === null)
+    {
+        name = "Guest";
+    }
+
+    return name;
+}
+
+/**
+ * Increments the level counter.
+ */
+function increaseLevel()
+{
+    var currentLevel = sessionStorage.getItem("currentLevel");
+
+    //If the current level is null, then initialize level counter.
+    if(currentLevel === null)
+    {
+        initializeLevelCounter();
+    }
+    //Otherwise increment level counter.
+    else
+    {
+        currentLevel++;
+    }
+    sessionStorage.setItem("currentLevel", currentLevel)
+}
+
+/**
+ * Returns the current level number.
+ */
+function getCurrentLevel()
+{
+    var currentLevel = sessionStorage.getItem("currentLevel");
+
+    //If the current level is null, then initialize level counter.
+    if(currentLevel === null)
+    {
+        currentLevel = 1;
+        initializeLevelCounter();
+    }
+
+    return currentLevel;
+}
+
+/**
+ * Stores an initial level value of 1.
+ */
+function initializeLevelCounter()
+{
+    sessionStorage.setItem("currentLevel", 1);
+}
+
+/**
+ * Retuns the current attempts at the level.
+ */
+function getLevelAttempts()
+{
+    var levelAttempts = sessionStorage.getItem("levelAttempts");
+
+    //If the value is null, then this is the first time that it is being called so
+    // reset value.
+    if(levelAttempts === null)
+    {
+        resetLevelAttempts();
+    }
+
+    return sessionStorage.getItem("levelAttempts");
+}
+
+/**
+ * Increments level counter.
+ */
+function increaseLevelAttempts()
+{
+    var levelAttempts = sessionStorage.getItem("levelAttempts");
+    
+    //If the value is null, then this is the first time that it is being called so 
+    // reset value.
+    if(levelAttempts === null)
+    {
+        resetLevelAttempts();
+    }
+    
+    //Increments counter then re-saves it.
+    levelAttempts++;
+    sessionStorage.setItem("levelAttempts", levelAttempts);
+}
+
+/**
+ * Resets the level counter back to 0.
+ */
+function resetLevelAttempts()
+{
+    sessionStorage.setItem("levelAttempts", 0);
 }
