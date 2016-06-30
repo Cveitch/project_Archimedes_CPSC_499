32bc24e4d1a096ddd0df2beac813bac32020907e
DavisGoulet <davis.goulet@gmail.com>
2016-06-29 20:53:11 -0700

    Added functions to pages and a formatting file.
    Moved info pertaining to the Splash_Page.js into its own file from the userinfo.js file. Added a formatting file which contains css for elemetns found on multiple pages.

---
+++ Score_Page.js
@@ -0,0 +1,42 @@
+/**
+ * Created by DavisGoulet on 2016-06-19.
+ */
+
+/**
+ * Loads the score page.
+ * @param {integer} levelAttempts
+ */
+function loadScorePage()
+{
+    window.location.href = "Score_Page.html";
+
+}
+
+/**
+ * When the score page loads, this function is called to set any variables onto the page.
+ */
+function updatePageInfo()
+{
+    var attemptString = "Attempts: " + getLevelAttempts();
+    var levelString = "Level " + getCurrentLevel() + " complete!";
+
+    document.getElementById("attemptMessage").innerHTML = attemptString;
+    document.getElementById("levelMessage").innerHTML = levelString;
+}
+
+
+/**
+ * Reloads the previous level.
+ */
+function replayLevel()
+{
+
+}
+
+/**
+ * Loads the next level.
+ */
+function nextLevel()
+{
+
+}
